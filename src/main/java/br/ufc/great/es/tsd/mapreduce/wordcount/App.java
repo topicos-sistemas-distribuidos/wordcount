6b3d8243404119413f1a3843b2a92bef5ace9719
armandossrecife <armando.sousa@gmail.com>
2019-05-17 17:43:39 -0300

    Criada primeira versao

---
+++ src/main/java/br/ufc/great/es/tsd/mapreduce/wordcount/App.java
@@ -0,0 +1,13 @@
+package br.ufc.great.es.tsd.mapreduce.wordcount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Teste do MapReduce!" );
+    }
+}
